08b1073ef9720e5d9036cb1f647efcc675792869
haroon760 <muhammadharoon2004059@gmail.com>
2025-11-25 11:43:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,301 @@
+// // chapter 1 (Q1) //
+
+// alert ("hello")
+
+// // chapter 1 (Q2) //
+
+// var fullName = ('muhammad \n haroon')
+
+// alert(fullName)
+
+// // chapter 1 (Q3) //
+
+// var fullName = ("Muhammad Haroon")
+
+// alert(fullName)
+
+// // chapter 1 (Q4) //
+
+// var fullName = ("Muhammad Haroon")
+
+// console.log(fullName)
+
+// // chapter 2 (Q1) //
+
+// var userName = ("Muhammad Haroon")
+
+// alert(userName)
+
+// // chapter 2 (Q2) //
+
+// var myName = ("Muhammad Haroon")
+
+// alert(myName)
+
+// // chapter 2 (Q3) //
+
+// var message = ("Hello World")
+
+// alert(message)
+
+// // chapter 2 (Q4) //
+
+// var students_name = ("Muhammad Haroon")
+// var student_age = (20)
+// var city = ("Karachi")
+
+// alert(student_age,students_name,city)
+
+// // chapter 2 (Q5) //
+
+// var myName = ("Haroon")
+
+// alert(myName)
+// alert(myName)
+// alert(myName)
+// alert(myName)
+// alert(myName)
+
+// // chapter 2 (Q6) //
+
+// var email = ("myemailaddressismuhammadharoon2004059@gmail.com")
+
+// alert(email)
+
+// // // chapter 2 (Q7) //
+
+// var book = ("I am trying to learn from the book A smarter way to learn JavaScript")
+
+// alert(book)
+
+// // // chapter 2 (Q8) //
+
+// var str = ("Hello World!")
+
+// alert(str)
+
+// chapter 3 (Q3)
+
+// let birthYear = 2004;
+// let message = "My birth year is" +  " " + birthYear + ".";
+
+// console.log(message)
+
+// chapter 3 (Q4)
+
+// let visitorName = ("JohnDoe")
+// let productTitle = ("T-shirts")
+// let quantity = (5)
+
+// console.log (visitorName,productTitle,quantity)
+
+// chapter 3 (Q9)
+
+// var age = 21
+
+// alert(age)
+
+// var mybirthyear = 2004
+
+// alert(mybirthyear)
+
+// chapter 4 (Q1)
+
+// let name = "Haroon", age = 21, city = "Karachi";
+
+// console.log(name,age,city)
+
+// chapter 4 (Q2)
+
+// legal variables names
+
+// let firstName;
+// let _age;
+// let $price;
+// let user123;
+// let totalAmount;
+
+// illegal variables names
+
+// let 123name;  
+// let user name;  
+// let @price;    
+// let let;        
+// let first-name; 
+
+// chapter 4 (Q3)
+// (a)
+// {/* <h1>Rules for naming JS variables</h1> */}
+
+// (b)
+
+// $my_1stVariable
+
+// (c)
+
+// my_Name
+// _1stName
+// my1stName
+
+// d
+
+// myVar
+
+// e
+
+// chapter 5 (Q1)
+
+// const num1 = 3
+// const num2 = 5
+
+// const sum = num1 + num2
+
+// const outputText = "The sum of " + num1 +  " and "  + num2 + " is " + sum
+
+// console.log (outputText)
+
+// chapter 5 (Q2)
+
+// const num1 = 8
+// const num2 = 5
+
+// const subtract = num1 - num2 
+
+// console.log(subtract)
+
+// const num1 = 8
+// const num2 = 5
+
+// const multiply = num1 * num2 
+
+// console.log(multiply)
+
+// const num1 = 8
+// const num2 = 5
+
+// const division = num1 / num2 
+
+// console.log(division)
+
+// const num1 = 8
+// const num2 = 5
+
+// const modulus = num1 % num2 
+
+// console.log(modulus)
+
+// chapter 5 (Q4)
+
+// const ticketPrice = 600;
+// const numberOfTickets = 5;
+
+// const totalCost = ticketPrice * numberOfTickets;
+
+// const outputText = " The cost to buy " + numberOfTickets + " movie tickets is " +  totalCost + " PKR. ";
+// console.log(outputText)
+
+// chapter 5 (Q5)
+
+// var tableNum = prompt("Enter Your Table Number" , 5)
+
+// console.log (tableNum , "x" , 1, "=" , tableNum * 1)
+// console.log (tableNum , "x" , 2, "=" , tableNum * 2)
+// console.log (tableNum , "x" , 3, "=" , tableNum * 3)
+// console.log (tableNum , "x" , 4, "=" , tableNum * 4)
+// console.log (tableNum , "x" , 5, "=" , tableNum * 5)
+// console.log (tableNum , "x" , 6, "=" , tableNum * 6)
+// console.log (tableNum , "x" , 7, "=" , tableNum * 7)
+// console.log (tableNum , "x" , 8, "=" , tableNum * 8)
+// console.log (tableNum , "x" , 9, "=" , tableNum * 9)
+// console.log (tableNum , "x" , 10, "=" , tableNum * 10)
+
+// chapter 6 (Q1)
+
+// let a = 10;
+// let output = "Result:\n\n"
+
+// output += "The value of a is: " + a + "\n";
+
+// // // pre-increment (++)
+
+// output += "The value of ++a is: " + (++a) + "\n";
+// output += " Now The value of a is: " + a + "\n";
+
+// // console.log (output)
+
+// // post-increment (++)
+
+// output += "The value of a++ is: " + (a++) + "\n";
+// output += " Now The value of a is: " + a + "\n";
+
+// // pre-decrement (--)
+
+// output += "The value of --a is: " + (--a) + "\n";
+// output += " Now The value of a is: " + a + "\n";
+
+// // post-decrement (--)
+
+// output += "The value of a-- is: " + (a--) + "\n";
+// output += " Now The value of a is: " + a + "\n";
+
+// console.log (output)
+
+// chapter 6 (Q6)
+
+// let sub1 = "Maths"
+// let sub2 = "Urdu"
+// let sub3 = "Isl"
+
+// let totalMarks = 100 * 3;
+
+// let firstSubObtainedMark = 58;
+// let secSubObtainedMark = 87;
+// let thirdSubObtainedMark = 77;
+
+// let totalObtainedMarks = firstSubObtainedMark + secSubObtainedMark + thirdSubObtainedMark
+
+// let percentage = (totalObtainedMarks / totalMarks) * 100
+
+// console.log("Total Marks --->",totalMarks)
+// console.log("Total Obtained Marks --->",totalObtainedMarks)
+// console.log("Total Percentage --->",percentage + "%")
+
+
+// document.writeln(` <table border="2">
+//         <tr>
+//             <th>Subject</th>
+//             <th>Total Marks</th>
+//             <th>Obtained Marks</th>
+//             <th>Percentage</th>
+//         </tr>
+//         <tr>
+//             <td>${sub1}</td>
+//             <td>100</td>
+//             <td>${firstSubObtainedMark}</td>
+            
+//         </tr>
+
+//         <tr>
+//             <td>${sub2}</td>
+//             <td>100</td>
+//             <td>${secSubObtainedMark}</td>
+            
+//         </tr>
+//         <tr>
+//             <td>${sub3}</td>
+//             <td>100</td>
+//             <td>${thirdSubObtainedMark}</td>
+            
+//         </tr>
+//         <tr>
+//             <th>Total</th>
+//             <td>${totalMarks}</td>
+//             <td>${totalObtainedMarks}</td>
+//             <td>${percentage}%</td>
+            
+            
+//         </tr>
+       
+//     </table>`)
+
+
